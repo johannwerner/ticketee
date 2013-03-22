3c0374c4fd99dbe708dd6b628374e3fadbe9e08f
Johann <johann@immedia.co.za>
2013-03-22 08:31:37 +0200

    Generated the Rails 3 application

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+gem 'rails', '3.1.0'
+gem 'sqlite3'
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+gem 'jquery-rails'
+
+group :test, :development do
+  gem 'rspec-rails', '~> 2.5'
+end
+group :test do
+  gem 'cucumber-rails'
+  gem 'capybara'
+  gem 'database_cleaner'
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = '554c6dd874b0ffd6c3d6ece58d7ad28bed4992ec72235cced33f705b5d671c374a357880b5e550ab8ca5c8bf1a459fb6c8809ba3b7687d58a28d7dfcf9c3cfe1'
